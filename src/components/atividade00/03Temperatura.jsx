fb88d5f4fcad2f7f6869b263c882788b17e40ecc
Heron Verissimo <heronverissimo4@gmail.com>
2023-03-21 14:14:35 -0300

    Correcao Atividade 20-03-2023

--- src/components/atividade00/03Temperatura.jsx
+++ src/components/atividade00/03Temperatura.jsx
@@ -1,23 +1,22 @@
 const Temperatura = () => {
-    const CtoF = (celsius) => {
-        return ((celsius * 1.8) + 32).toFixed(2)
-    }
-    const FtoC = (fahrenheit) => {
-        return ((fahrenheit - 32) / 1.8).toFixed(2)
-    }
+    // const CtoF = (celsius) => {
+    //     return ((celsius * 1.8) + 32).toFixed(2)
+    // }
+    const CtoF = (celsius) => ((celsius * 1.8) + 32).toFixed(2)
+    const FtoC = (fahrenheit) => ((fahrenheit - 32) / 1.8).toFixed(2)
     const KtoCF = (kelvin) => {
         const celsius = kelvin - 273
         const fahrenheit = CtoF(celsius)
         return {celsius, fahrenheit}
     }
+    const n = 25
+    const {KtoC, KtoF} = KtoCF(n)
     return(
         <div>
-           <h3>25C em F é {CtoF(25)}</h3>
-           <h3>25F em C é {FtoC(25)}</h3>
-           <h3>25K em C é {KtoCF(25).celsius}</h3> 
-           <h3>25K em F é {KtoCF(25).fahrenheit}</h3> 
-           <h2>Teste com git hub 2 </h2>
-           <h2>teste git 3 </h2>
+           <h3>{n} °C em °F é {CtoF(n)}</h3>
+           <h3>{n} °F em °C é {FtoC(n)}</h3>
+           <h3>{n} K em °C é {KtoC}</h3> 
+           <h3>{n} K em °F é {KtoF}</h3> 
         </div>
     )
 
